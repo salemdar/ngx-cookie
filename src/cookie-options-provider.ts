980447a224edec7de7f528c7ae331c570825737f
Hugo Campos <hugo.campos@pix4d.com>
2017-09-29 09:35:55 +0200

    fix(opaque-token): replace usage of OpaqueToken with InjectionToken

--- src/cookie-options-provider.ts
+++ src/cookie-options-provider.ts
@@ -1,25 +1,23 @@
-import { Injectable, Injector, Inject, OpaqueToken } from '@angular/core';
+import { Injectable, Injector, Inject, InjectionToken } from '@angular/core';
 import { APP_BASE_HREF } from '@angular/common';
 
 import { CookieOptions } from './cookie-options.model';
 import { mergeOptions } from './utils';
 
-export const COOKIE_OPTIONS = new OpaqueToken('COOKIE_OPTIONS');
+export const COOKIE_OPTIONS = new InjectionToken('COOKIE_OPTIONS');
 
 /** @private */
 @Injectable()
 export class CookieOptionsProvider {
-
   private defaultOptions: CookieOptions;
   private _options: CookieOptions;
 
-  constructor(@Inject(COOKIE_OPTIONS) options: CookieOptions = {},
-              private _injector: Injector) {
+  constructor(@Inject(COOKIE_OPTIONS) options: CookieOptions = {}, private _injector: Injector) {
     this.defaultOptions = {
       path: this._injector.get(APP_BASE_HREF, '/'),
       domain: null,
       expires: null,
-      secure: false
+      secure: false,
     };
     this._options = mergeOptions(this.defaultOptions, options);
   }
